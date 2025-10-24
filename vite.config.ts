1f57789cfd73bd1b981d52727c03661a77022624
Miriam <miriam.mr90@gmail.com>
2025-10-24 13:49:04 +0200

    Fix Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: process.env.NODE_ENV === 'production' ? '/huckleberry/' : '/',
   plugins: [
     react(),
     mode === 'development' &&
